df6c3b3443ccf710a7ab9ab8e32e224b46eeb0ce
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-11 12:30:17 +0000

    Adding new file

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from datetime import datetime
 class BaseModel():
     """This class represents a basemodel"""
     def __init__(self):
+        """Instantiation of the class and assigning values to its attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().isoformat()
         self.updated_at = datetime.now().isoformat()
