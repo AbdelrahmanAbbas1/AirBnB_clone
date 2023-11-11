875da71db86421a2e0c24bbb31fcff042d461abe
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-11 20:18:25 +0000

    Adding new file

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel():
     def __init__(self):
         """Instantiation of the class and assigning values to its attributes"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Retrun string representation of the instance"""
@@ -22,5 +22,8 @@ class BaseModel():
 
     def to_dict(self):
         """Returns a dictionary with all keys/values of __dict__"""
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
