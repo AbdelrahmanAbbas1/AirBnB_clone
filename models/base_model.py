7c1cde7b66e037bc666d9878656c0850489df5d5
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-11 20:56:28 +0000

    Adding new file

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 
 class BaseModel():
     """This class represents a basemodel"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiation of the class and assigning values to its attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k == "__class__":
+                    continue
+                self.__dict__[k] = v
 
     def __str__(self):
         """Retrun string representation of the instance"""
@@ -22,8 +29,8 @@ class BaseModel():
 
     def to_dict(self):
         """Returns a dictionary with all keys/values of __dict__"""
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        full_dict = self.__dict__.copy()
+        full_dict["created_at"] = self.created_at.isoformat()
+        full_dict["updated_at"] = self.updated_at.isoformat()
+        full_dict["__class__"] = self.__class__.__name__
+        return full_dict
